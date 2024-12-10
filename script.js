95fb9ceb282cebb04b912d09906d148ba0c2fd5c
acholke1 <118831793+acholke1@users.noreply.github.com>
2024-12-10 23:17:15 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.nav-menu');
-
-toggle.addEventListener('click', () => {
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-});
